392a906048e8325731cc0458b87acb82ab651bf9
milkbobo <mileto@qq.com>
2015-03-12 14:01:52 +0800

    格式时间
    格式时间

--- Public/Js/edit.js
+++ Public/Js/edit.js
@@ -66,10 +66,10 @@ $(function () {
 		onUploadSuccess : function (file, data, response) {
 			eval('var data = ' + data);	
 			if (data.status) {
-			$('#face-img').attr('src', ROOT+'/Uploads/Face/'+data.path.m180);
-			$('input[name=face180]').val(data.path.m180);
-			$('input[name=face80]').val(data.path.m80);
-			$('input[name=face50]').val(data.path.m50);
+			$('#face-img').attr('src', ROOT+'/Uploads/Face/'+data.path.max);
+			$('input[name=face180]').val(data.path.max);
+			$('input[name=face80]').val(data.path.medium);
+			$('input[name=face50]').val(data.path.mini);
 		} else {
 			alert(data.msg);
 		}		
@@ -95,7 +95,7 @@ $(function () {
 		},
 		rules : {
 			old : {
-				required : true,
+				required : true,//不能为空
 				user : true
 			},
 			new : {
