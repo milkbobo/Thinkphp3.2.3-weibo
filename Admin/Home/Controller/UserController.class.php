945c17c3f148066da140206aab4c159958f47d96
milkbobo <mileto@qq.com>
2015-04-24 15:13:53 +0800

    微博用户检索与锁定用户功能实现
    微博用户检索与锁定用户功能实现

--- Admin/Home/Controller/UserController.class.php
+++ Admin/Home/Controller/UserController.class.php
@@ -14,7 +14,7 @@ class UserController extends CommonController {
     	$Page       = new \Think\Page($count,20);// 实例化分页类 传入总记录数和每页显示的记录数(20)
     	// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
     	$limit=$Page->firstRow.','.$Page->listRows;
-    	$Page->setConfig('theme',"共 %TOTAL_ROW% 条记录 %FIRST% %UP_PAGE% %NOW_PAGE% / %TOTAL_PAGE% %DOWN_PAGE% %END% ");
+    	$Page->setConfig('theme',"共 %TOTAL_ROW% 条记录 %FIRST% %UP_PAGE% %NOW_PAGE% / %TOTAL_PAGE% 页 %DOWN_PAGE% %END% ");
     	$Page->setConfig('prev','上一页');
     	$Page->setConfig('next','下一页');
     	
@@ -23,4 +23,65 @@ class UserController extends CommonController {
     	
     	$this->display();
     }
+    
+    /*
+     * 锁定用户
+     */
+    public function lockUser(){
+    	$data = array(
+    			'id'=>I('id','','intval'),
+    			'lock'=>I('lock','','intval')
+    			);
+    	
+    	$msg = $data['lock'] ? '锁定' : '解锁';
+    	if(M('user')->save($data)){
+    		$this->success($msg.'成功',$_SERVER['HTTP_REFERE']);//$_SERVER['HTTP_REFERE'] 提交时候的页面，连接的来源地址
+    	}else {
+    		$this->error($msg.'失败，请重试');
+    	}
+    }
+    
+    /*
+     * 微博用户检索
+     */
+    
+    public function sechUser(){
+    	if(isset($_GET['sech']) && isset($_GET['type'])){
+    		$where = array($where=$_GET['type'] ? array('user.id'=>I('sech','','intval')) : array('username'=>array('LIKE','%'.I('sech').'%')));
+    		$user =D('UserView')->where($where)->select();
+    		$this->user=$user ? $user : false;
+    	}
+    	$this->display();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
