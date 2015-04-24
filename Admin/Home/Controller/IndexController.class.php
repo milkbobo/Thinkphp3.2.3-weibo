1b418dc80c77c56594e7b1008acdb4c4e3ad56cd
milkbobo <mileto@qq.com>
2015-04-24 11:18:40 +0800

    通过PHP常量获取服务器信息并通过TP的模版输出标签输出到页面当中
    通过PHP常量获取服务器信息并通过TP的模版输出标签输出到页面当中

--- Admin/Home/Controller/IndexController.class.php
+++ Admin/Home/Controller/IndexController.class.php
@@ -9,6 +9,7 @@ class IndexController extends CommonController {
 	 * 后台首页视图
 	 */
     public function index(){
+    	//p($_SERVER);
     	$this->display();
     }
     
@@ -16,6 +17,15 @@ class IndexController extends CommonController {
      * 后台信息首页
      */
     public function copy(){
+    	//p($_SESSION);
+    	$db=M('user');
+    	$this->user=$db->count();
+    	$this->lock=$db->where(array('lock'=>1))->count();
+
+    	$db=M('weibo');
+    	$this->weibo = $db->where(array('isturn'=>0))->count();
+    	$this->turn = $db->where(array('isturn'=>array('GT',0)))->count();//isturn大于0的是转发微博
+    	$this->comment = M('comment')->count();
     	$this->display();
     }
     
